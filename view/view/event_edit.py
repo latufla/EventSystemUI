bed41ff70f6a5e1ae0f65c1d8e6e06c897cea667
latufla <lafufla.work@gmail.com>
2018-01-05 14:49:22 +0700

    event_edit, action url

--- view/view/event_edit.py
+++ view/view/event_edit.py
@@ -2,10 +2,10 @@ from view.data.event import Event
 
 
 class View:
-    def __init__(self, event: Event, action_url: str):
+    def __init__(self, event: Event, save_url: str):
         self.event = event
 
-        self.action_url = action_url
+        self.save_url = save_url
 
         self.min_width = 400
         self.max_width = 1200
